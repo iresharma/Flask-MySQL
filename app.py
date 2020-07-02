8d6d3548327d5d284f589bb3123d872d5d5c7d49
saxenamanas0 <saxenamanas0@gmail.com>
2020-07-03 02:32:48 +0530

    Added pages

--- app.py
+++ app.py
@@ -7,10 +7,25 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/login')
+def login():
     return render_template('login.html')
 
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/teacher')
+def teacher():
+    return render_template('teacher.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
