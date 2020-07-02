ad38080b3c78716fa482d4fbbc537cdea17f0ec3
iresh sharma <iresh.sharma8@gmail.com>
2020-07-02 23:49:56 +0530

    initialize

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+from flask import redirect
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home:
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=true)
