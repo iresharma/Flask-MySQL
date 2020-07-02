e92e3894f3a79dabd3fcaafd04fb99b5118cb2f4
saxenamanas0 <saxenamanas0@gmail.com>
2020-07-03 03:32:07 +0530

    Added pages and routes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import render_template
 from flask import url_for
 from flask import redirect
 from flask import request
-
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
@@ -26,6 +26,26 @@ def register():
 @app.route('/student')
 def student():
     return render_template('student.html')
+
+@app.route('/student/viewgames')
+def viewgames():
+    return render_template('viewgames.html')
+
+@app.route('/student/pastgames')
+def pastgames():
+    return render_template('pastgame.html')
+
+@app.route('/teacher/showgameslist')
+def showgameslist():
+    return render_template('showgameslist.html')
+
+@app.route('/teacher/createagame')
+def createagame():
+    return render_template('creategame.html')
+
+@app.route('/teacher/gamedetails')
+def gamedetails():
+    return render_template('gamedetails.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
